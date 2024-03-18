5a931438fb49385c519efee1d64274a2cf6494f2
CarlosRussell13 <133693000+CarlosRussell13@users.noreply.github.com>
2024-03-18 21:30:22 +0000

    done

--- supabase.js
+++ supabase.js
@@ -3,16 +3,15 @@ const supabaseUrl = 'https://ifcadoorzgtfmwjrjeau.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function getbooks() {
-let { data: lab 8, error } = await supabase
-  .from('lab 8')
+async function getBooks() {
+  let { data: books, error } = await supabase
+  .from('books')
   .select('*')
 
-
-for (let book of books) {
-  let bookList = document.getElementById('lab 8');
-  bookList.innerHTML += `<tr><td>${book.title} <td>${book.author} <td> ${book.ISBN}`;
-}
+  for (let book of books) {
+      let bookList = document.getElementById('books');
+      bookList.innerHTML += `<tr><td>${book.title} <td>${book.author} <td> ${book.ISBN}`;
+  }
 }
 
 getBooks();
