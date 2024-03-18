c8dbdf80275158daf8eadf637717177daf7e4346
CarlosRussell13 <133693000+CarlosRussell13@users.noreply.github.com>
2024-03-18 21:38:33 +0000

    done (fixed table showing up in one sentence)

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://ifcadoorzgtfmwjrjeau.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js@2.7.1'
+
+const supabaseUrl = 'https://cbjemlnnfxbvvvmlvpzk.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNiamVtbG5uZnhidnZ2bWx2cHprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NDIzNjUsImV4cCI6MjAyNjMxODM2NX0.uwlONI4ORs64NbcnzLKwO_Nhn0n7-bRidKEz2pNWUrI'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function getBooks() {
